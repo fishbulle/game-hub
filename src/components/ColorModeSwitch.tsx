7a3b3a1005626960bdb47f7aaf575a56b61169bb
Angelina <linaemalmros@gmail.com>
2023-04-10 15:17:49 +0200

    changed color of color mode switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,7 +5,7 @@ const ColorModeSwitch = () => {
 
     return (
         <HStack>
-            <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+            <Switch colorScheme='red' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
             <Text>Dark Mode</Text>
         </HStack>
     )
